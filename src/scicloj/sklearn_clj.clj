d5b0563ba209d3f495cd7864bbe220c5618f5136
Carsten Behring <carsten.behring@gmail.com>
2021-02-10 16:57:16 +0100

    adapted to metamorph
    add tests
    cleaned up

--- src/scicloj/sklearn_clj.clj
+++ src/scicloj/sklearn_clj.clj
@@ -1,8 +1,8 @@
 (ns scicloj.sklearn-clj
   (:require [camel-snake-kebab.core :as csk]
             libpython-clj.metadata
-            [libpython-clj.python :refer [->jvm ->numpy call-kw py.]]
-            [tablecloth.api :as tc]
+            [libpython-clj.python :refer [->jvm ->numpy call-kw py.- py.]]
+            [tech.v3.dataset :as ds]
             [tech.v3.dataset.column-filters :as cf]
             [tech.v3.dataset.modelling :as ds-mod]
             [tech.v3.dataset.tensor :as dst]
@@ -18,119 +18,116 @@
 
 
 
-(defn fit-transform
-  [ds module-kw estimator-class-kw kw-args
-   ]
-  (def ds ds)
-  (user/def-let
-    [inference-targets (cf/target ds)
-     feature-ds (cf/feature ds)
-     snakified-kw-args (snakify-keys kw-args)
-     module (csk/->snake_case_string module-kw)
-     class-name (csk/->PascalCaseString estimator-class-kw)
-     estimator-class-name (str "sklearn." module "." class-name)
-     constructor (libpython-clj.metadata/path->py-obj estimator-class-name )
-     estimator (call-kw constructor [] kw-args)
-     X (-> feature-ds (dst/dataset->tensor) ->numpy)]
-    (->
-     (py. estimator fit_transform X )
-     (t/ensure-tensor)
-     ;; ->jvm
-     (dst/tensor->dataset)
-     (tc/append inference-targets)
-     )
-    ))
-
-
 
 (defn fit
-  [ds module-kw estimator-class-kw kw-args
-   ]
+  "Call the fit method of a sklearn transformer, which is specified via
+   the two keywords `module-kw` and `estimator-class-kw`.
+  Keyword arguments can be given in a map `kw-arguments`.
+  The data need to be given as tech.ml.dataset and will be converted to pythjon automaticaly.
+  The function will return the estimator as a python object.
+  "
+  [ds module-kw estimator-class-kw kw-args]
   (let
-    [inference-targets (cf/target ds)
-     feature-ds (cf/feature ds)
-     snakified-kw-args (snakify-keys kw-args)
-     module (csk/->snake_case_string module-kw)
-     class-name (csk/->PascalCaseString estimator-class-kw)
-     estimator-class-name (str "sklearn." module "." class-name)
-     constructor (libpython-clj.metadata/path->py-obj estimator-class-name )
-     estimator (call-kw constructor [] kw-args)
-     X (-> feature-ds (dst/dataset->tensor) ->numpy)
-     y (-> inference-targets (dst/dataset->tensor) ->numpy)]
-    (py. estimator fit X y)
-    ))
+      [inference-targets (cf/target ds)
+       feature-ds (cf/feature ds)
+       snakified-kw-args (snakify-keys kw-args)
+       module (csk/->snake_case_string module-kw)
+       class-name (csk/->PascalCaseString estimator-class-kw)
+       estimator-class-name (str "sklearn." module "." class-name)
+       constructor (libpython-clj.metadata/path->py-obj estimator-class-name )
+       estimator (call-kw constructor [] kw-args)
+       X (-> feature-ds (dst/dataset->tensor) ->numpy)]
+      (if (nil? inference-targets)
+        (py. estimator fit X)
+        (let [y (-> inference-targets (dst/dataset->tensor) ->numpy) ]
+          (py. estimator fit X y)))))
 
 (defn predict
+  "Calls `predict` on the given sklearn estimator object, and returns the result as a tech.ml.dataset"
   [ds estimator kw-args]
   (let
-    [
-     feature-ds (cf/feature ds)
-     snakified-kw-args (snakify-keys kw-args)
-     X (-> feature-ds (dst/dataset->tensor) ->numpy)
-     y_hat
-     (->
-      (py. estimator predict X)
-      (t/ensure-tensor)
-      ->jvm
-      (dst/tensor->dataset)
-      )]
-    (tc/append feature-ds y_hat)
-
-
-    ))
-
-
-
+      [
+       feature-ds (cf/feature ds)
+       inference-target-column-names (ds-mod/inference-target-column-names ds)
+       snakified-kw-args (snakify-keys kw-args)
+       X (-> feature-ds (dst/dataset->tensor) ->numpy)
+       y_hat
+       (->
+        (py. estimator predict X)
+        (t/ensure-tensor)
+        ->jvm
+        (dst/tensor->dataset))
+       y_hat (ds/rename-columns y_hat (zipmap (ds/column-names y_hat) inference-target-column-names))]
+    (ds/append-columns feature-ds (ds/columns y_hat))))
 
 (defn transform
+  "Calls `transform` on the given sklearn estimator object, and returns the result as a tech.ml.dataset"
   [ds estimator kw-args]
-  (let [
-        snakified-kw-args (snakify-keys kw-args)
-
-     feature-ds (cf/feature ds)
-        X (-> feature-ds (dst/dataset->tensor) ->numpy)]
-    (->
-     (py. estimator transform X )
-      (t/ensure-tensor)
-      ->jvm
-      (dst/tensor->dataset)
-     )
-    ))
+  (let [snakified-kw-args (snakify-keys kw-args)
+        feature-ds (cf/feature ds)
+        column-names (ds/column-names feature-ds)
+        X (-> feature-ds (dst/dataset->tensor) ->numpy)
+        X-transformed
+        (-> (py. estimator transform X )
+            (t/ensure-tensor)
+            ->jvm
+         (dst/tensor->dataset))]
+    (ds/rename-columns
+     X-transformed
+     (zipmap
+      (ds/column-names X-transformed)
+      column-names))))
 
 
 
 
 
 (comment
+
+  (require
+   '[libpython-clj.python :refer [py.-]]
+   '[tech.v3.dataset :as ds]
+   '[tech.v3.dataset.modelling :as ds-mod])
+
   (def train-ds
     (->
-     (tc/dataset {:x1 [1 2 3]
-                  :x2  [7 8 9]
-                  :y [10 10 10 ]})
-     (ds-mod/set-inference-target :y)
-     ))
+     (ds/->dataset {:x1 [1 1 2 2]
+                  :x2 [1 2 2 3]
+                  :y  [6 8 9 11]})
 
-  (def test-ds (->
-                (tc/dataset {:x1 [0 0 0]
-                             :x2  [2 4 6]
-                             :y [0 0 0 ]})
-                (ds-mod/set-inference-target :y)
-                ))
+     (ds-mod/set-inference-target :y)))
+
+  (def test-ds
+    (->
+     (ds/->dataset {:x1 [3]
+                  :x2 [5]
+                  :y  [0]})
+     (ds-mod/set-inference-target :y)))
 
   (def lin-reg
-    (fit train-ds :linear_model :linear-regression ;; {:y [ 9 8 7]}
-         {}
-         ))
+    (fit train-ds :linear_model :linear-regression {}))
+
   (predict test-ds lin-reg {})
+  ;; => _unnamed [1 3]:
+  ;;    | :x1 | :x2 |   :y |
+  ;;    |-----|-----|------|
+  ;;    |   3 |   5 | 16.0 |
 
 
-
-
+  (def data
+    (ds/->dataset {:x1 [0 0 1 1]
+                 :x2 [0 0 1 1]}))
   (def scaler
-    (fit train-ds :preprocessing :standard-scaler {})
+    (fit data :preprocessing :standard-scaler {}))
 
-    )
+  (py.- scaler mean_)
+  ;; => [0.5 0.5]
+  ;;
+  (transform (ds/->dataset {:x1 [2] :x2 [2]})  scaler {})
+  ;; => :_unnamed [1 2]:
+  ;;    | :x1 | :x2 |
+  ;;    |-----|-----|
+  ;;    | 3.0 | 3.0 |
 
-  (transform test-ds scaler {})
 
   )
--- src/scicloj/sklearn_clj/metamorph.clj
+++ src/scicloj/sklearn_clj/metamorph.clj
@@ -8,57 +8,20 @@
   )
 
 
-(ns scicloj.sklearn_clj.metamorph
+(ns scicloj.sklearn-clj.metamorph
   (:require [tech.v3.dataset.modelling :as ds-mod]
-            [tablecloth.api :as tc]
-
-            [sciloj.sklearn-clj :as sklearn]
+            [scicloj.sklearn-clj :as sklearn]
             ))
 
-(defn fit-or-predict [pipeline-ctx module-kw estimator-class-kw kw-args]
-  (def pipeline-ctx pipeline-ctx)
-  (let [ds (:dataset pipeline-ctx)]
-    (case (:mode pipeline-ctx)
-      :fit (assoc pipeline-ctx
-                  :sciloj.sklearn.stadapip/model
+(defn estimate [ctx module-kw estimator-class-kw kw-args]
+  (let [ds (:metamorph/dataset ctx)
+        id (:metamorph/id ctx)]
+    (case (:metamorph/mode ctx)
+      :fit (assoc ctx
+                  id
                   (sklearn/fit ds module-kw estimator-class-kw kw-args ))
-      :transform (assoc pipeline-ctx
-                        :dataset
-                        (sklearn/predict
-                         (:dataset pipeline-ctx)
-                         (:sciloj.sklearn.stadapip/model pipeline-ctx)
-                         kw-args
-                         )))))
-
-
-(comment
-
-
-  (defn pipeline [ctx]
-    (-> ctx
-        ((fn [ctx]
-           (assoc ctx :dataset
-                  (ds-mod/set-inference-target (:dataset ctx) :y))
-           ))
-        (fit-or-predict :linear-model :linear-regression {})))
-
-
-  (def fitted
-    (pipeline
-     {:mode :fit
-      :dataset
-      (tc/dataset {:x1 [1 2 3]
-                   :x2  [7 8 9]
-                   :y [5 1 13 ]})}))
-
-
-  (def prediction
-    (pipeline
-     (merge fitted
-            {:mode :transform
-             :dataset
-             (tc/dataset {:x1 [-6 1 2 1 -5 ]
-                          :x2  [1 2 8 9 -2]
-                          :y [0 0 0 0]})})))
-
-  )
+      :transform (assoc ctx
+                        :metamorph/dataset
+                        (sklearn/predict ds
+                         (get ctx id)
+                         kw-args)))))
