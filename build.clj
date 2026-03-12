19be08e6f3f0acad8de8a2a86d452fb2a8c96830
Carsten Behring <carsten.behring@gmail.com>
2026-03-12 17:09:35 +0000

    use uv
    added release pipeline

--- build.clj
+++ build.clj
@@ -7,15 +7,18 @@
 ; alternatively, use MAJOR.MINOR.COMMITS:
 ;;(def version (format "0.2.%s" (b/git-count-revs nil)))
 (def version "0.5")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def class-dir "target/classes")
+
 
 (defn test "Run the tests." [opts]
   (-> opts
-      (assoc :aliases [:runner])
+      (assoc :aliases [:test :runner])
       (bb/run-tests)))
 
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
   (-> opts
-      (assoc :lib lib :version version :aliases [:runner])
+      (assoc :lib lib :version version :aliases [:test :runner])
       (bb/run-tests)
       (bb/clean)
       (bb/jar)))
@@ -34,3 +37,26 @@
   (-> opts
       (assoc :lib lib :version version)
       (bb/deploy)))
+
+(defn- pom-template []
+  [[:description "Plugin to use sklearn models in metamorph.ml"]
+   [:url "https://github.com/scicloj/scicloj.ml.xgboost"]
+   [:licenses
+    [:license
+     [:name "Eclipse Public License - v 1.0"]
+     [:url "https://www.eclipse.org/legal/epl-1.0/"]]]
+   [:scm
+    [:url "https://github.com/scicloj/sklearn-clj"]
+    [:connection "scm:git:https://github.com/scicloj/sklearn-clj.git"]
+    [:developerConnection "scm:git:https://github.com/scicloj/sklearn-clj.git"]
+    [:tag (str "v" version)]]])
+
+
+(defn generate-pom [_]
+  (b/write-pom {:class-dir class-dir
+                :target "."
+                :lib lib
+                :version version
+                :basis basis
+                :pom-data (pom-template)
+                :src-dirs ["src"]}))
